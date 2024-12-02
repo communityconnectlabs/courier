b183d597bbdc0c02533b5162abfbd5222dcfeb84
noizu <keith.brings@noizu.com>
2024-12-03 01:10:59 +0700

    entrypoint.sh bin path fix.

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /app || exit 1
-go install -v ./cmd/courier && chmod +x ./courier || exit 1
-./courier
+go install -v ./cmd/courier && chmod +x /go/bin/courier || exit 1
+/go/bin/courier
